49284621ab39215670f7da46f36518101a779cf7
Tom Montagnon <tom.montagnon@protonmail.com>
2025-07-16 11:51:32 +0200

    suppression des source ros imported

--- zsh/cfg/ros.zsh
+++ zsh/cfg/ros.zsh
@@ -1,34 +1,3 @@
-source_tiara() {
-	source ~/projects/ros/tiara_workspace/devel/setup.zsh
-	export ROSCONSOLE_FORMAT='[${severity}] ${node}: ${message}'
-	export GAZEBO_RESOURCE_PATH="$HOME/data/tiara/gazebo:$HOME/data/romea/gazebo"
-	export GAZEBO_MODEL_PATH="$HOME/data/tiara/gazebo/models:$HOME/data/romea/gazebo/models"
-}
-
-source_tiara2() {
-  unset ROS_DISTRO
-  source /opt/ros/galactic/setup.zsh
-  source ~/projects/ros/tiara_ros2/install/local_setup.zsh
-  source /usr/share/colcon_cd/function/colcon_cd.sh
-  eval "$(register-python-argcomplete3 ros2)"
-  eval "$(register-python-argcomplete3 colcon)"
-  export RCUTILS_CONSOLE_OUTPUT_FORMAT="[{severity}] {name}: {message}"
-	export GAZEBO_RESOURCE_PATH="$HOME/data/tiara/gazebo:$HOME/data/romea/gazebo"
-	export GAZEBO_MODEL_PATH="$HOME/data/tiara/gazebo/models:$HOME/data/romea/gazebo/models"
-  export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-}
-
-source_romea2() {
-  unset ROS_DISTRO
-  source /usr/share/gazebo/setup.sh
-  source /opt/ros/galactic/setup.zsh
-  source ~/projects/ros/romea_ros2/install/local_setup.zsh
-  source /usr/share/colcon_cd/function/colcon_cd.sh
-  eval "$(register-python-argcomplete3 ros2)"
-  eval "$(register-python-argcomplete3 colcon)"
-  export RCUTILS_CONSOLE_OUTPUT_FORMAT="[{severity}] {name}: {message}"
-}
-
 source_tirrex() {
   unset ROS_DISTRO
   source /usr/share/gazebo/setup.sh
@@ -43,6 +12,12 @@ source_tirrex() {
 	export GAZEBO_MODEL_PATH="$GAZEBO_MODEL_PATH:$HOME/data/romea/gazebo/models"
 }
 
+src_ros() {
+  source /opt/ros/jazzy/setup.zsh
+  export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST
+  eval "$(register-python-argcomplete ros2)"
+  eval "$(register-python-argcomplete colcon)"
+}  
+
 alias ccd="colcon_cd"
 alias ccb="colcon build --symlink-install"
-alias src_ros="source /opt/ros/jazzy/setup.zsh; export ROS_DOMAIN_ID=4; export ROS_AUTOMATIC_DISCOVERY_RANGE=LOCALHOST"
